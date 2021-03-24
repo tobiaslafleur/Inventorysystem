f9e7c3a43e2c2f4954a47009721395c7a237f8bd
unganemo <76483254+unganemo@users.noreply.github.com>
2021-03-24 17:52:13 +0100

    moved templogin to temp dbController

---
+++ src/Controller/DatabaseController.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Model.User;
+
+public class DatabaseController {
+    private MainController mainController;
+
+    public DatabaseController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    public boolean checkUser(User user) {
+
+        if(user.getUsername().equals("inventory") && user.getPassword().equals("system")){
+            user.setUserID(1);
+            return true;
+        } else {
+            user.setUsername(null);
+            user.setPassword(null);
+            user.setUserID(-1);
+            return false;
+        }
+    }
+}
--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -5,9 +5,11 @@ import View.*;
 
 public class MainController {
     private TempMainFrame mainFrame;
+    private DatabaseController dbController;
 
     public MainController(){
         mainFrame = new TempMainFrame(this);
+        dbController = new DatabaseController(this);
     }
 
     public void setUser(String username, char[] password) {
@@ -18,22 +20,7 @@ public class MainController {
         /*-------------------------------------------------------------------------------------*/
 
         //How it should work (temporarily)
-        /*if(dbController.checkUser(user)) {
-            //proceed with login
-            //sets userID in dbController.checkUser();
-            mainFrame.updateLogin();
-        } else {
-            user.setUsername(null);
-            user.setPassword(null);
-            user.setUserID(-1);
-            mainFrame.showMsg("Wrong login");
-        }*/
-
-        /*-------------------------------------------------------------------------------------*/
-
-        /*Temp login*/
-        if(user.getUsername().equals("inventory") && user.getPassword().equals("system")){
-            user.setUserID(1);
+        if(dbController.checkUser(user)) {
             mainFrame.updateLogin();
         } else {
             user.setUsername(null);
