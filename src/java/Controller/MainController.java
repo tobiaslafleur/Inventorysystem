561b41c43ba14f5c80ef2fea9274501b119c1ecf
Tobias la Fleur <tobias.lafleur98@gmail.com>
2021-04-06 15:24:24 +0200

    connect to database

--- src/java/Controller/MainController.java
+++ src/java/Controller/MainController.java
@@ -1,36 +1,12 @@
 package Controller;
 
-import Model.*;
 import View.*;
 
 public class MainController {
-    private TempMainFrame mainFrame;
     private DatabaseController dbController;
 
     public MainController(){
-        mainFrame = new TempMainFrame(this);
         dbController = new DatabaseController(this);
     }
 
-    public void setUser(String username, char[] password) {
-        User user = new User(username, String.valueOf(password));
-
-        System.out.println(user);
-
-        /*-------------------------------------------------------------------------------------*/
-
-        //How it should work (temporarily)
-        if(dbController.checkUser(user)) {
-            mainFrame.updateLogin();
-        } else {
-            user.setUsername(null);
-            user.setPassword(null);
-            user.setUserID(-1);
-            mainFrame.showMsg("Wrong login");
-        }
-
-        /*-------------------------------------------------------------------------------------*/
-
-        System.out.println(user);
-    }
 }
