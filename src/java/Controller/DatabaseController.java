334a977ee2c4d4a6e3a22967960a5ec67da9cc13
Tobias la Fleur <47904792+tobiaslafleur@users.noreply.github.com>
2021-04-06 15:24:39 +0200

    connect to database (#8)

--- src/java/Controller/DatabaseController.java
+++ src/java/Controller/DatabaseController.java
@@ -1,24 +1,31 @@
 package Controller;
 
-import Model.User;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseController {
     private MainController mainController;
 
+    private Connection conn;
+
     public DatabaseController(MainController mainController) {
         this.mainController = mainController;
+
+        connectToDatabase();
     }
 
-    public boolean checkUser(User user) {
+    public void connectToDatabase() {
+        try {
+            String dbUrl = "jdbc:sqlserver://ecinv.database.windows.net;databaseName=ecinv;";
+            String user = "ecinv";
+            String password = "mau123456!";
 
-        if(user.getUsername().equals("inventory") && user.getPassword().equals("system")){
-            user.setUserID(1);
-            return true;
-        } else {
-            user.setUsername(null);
-            user.setPassword(null);
-            user.setUserID(-1);
-            return false;
+            conn = DriverManager.getConnection(dbUrl, user, password);
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
+
 }
