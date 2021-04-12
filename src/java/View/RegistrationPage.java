92505c0c26e6030fa798ac11c9455a372721af88
unganemo <76483254+unganemo@users.noreply.github.com>
2021-04-12 15:06:17 +0200

    felhantering i register

--- src/java/View/RegistrationPage.java
+++ src/java/View/RegistrationPage.java
@@ -10,6 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 import java.io.IOException;
 
 public class RegistrationPage {
@@ -35,18 +37,31 @@ public class RegistrationPage {
     }
 
     public void register(javafx.event.ActionEvent event) {
-        facilitator.createUser(username.getText(),password.getText(), email.getText(), phone.getText(), address.getText());
+        //Check if all fields are entered
+        if(!username.getText().isEmpty() && !password.getText().isEmpty() && !email.getText().isEmpty() && !phone.getText().isEmpty() && !address.getText().isEmpty() && !repeatedPW.getText().isEmpty()){
+            //Check if password is same as repeated password
+            if(password.getText().equals(repeatedPW.getText())){
+                facilitator.createUser(username.getText(),password.getText(), email.getText(), phone.getText(), address.getText());
+                try {
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/loginPage.fxml"));
+                    Scene scene = new Scene(root);
+                    Node button =(Node) event.getSource();
+                    Stage stage = (Stage) button.getScene().getWindow();
 
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/loginPage.fxml"));
-            Scene scene = new Scene(root);
-            Node button =(Node) event.getSource();
-            Stage stage = (Stage) button.getScene().getWindow();
-
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //Display text: Passwords must match
+                //Temp:
+                JOptionPane.showMessageDialog(null, "Passwords must match");
+            }
+        } else {
+            //Display text: "Enter all required fields"
+            //Temp:
+            JOptionPane.showMessageDialog(null, "Enter all required fields.");
         }
     }
 }
