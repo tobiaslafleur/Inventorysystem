324382a3f3d0cdd0ed3552bae34550d24147e375
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-14 11:14:54 +0200

    Added Supplier SQL & Testfile (#22)

---
+++ src/java/Tests/SupplierSQLTest.java
@@ -0,0 +1,16 @@
+package Tests;
+import Controller.*;
+import Controller.Database.DBProduct;
+
+public class SupplierSQLTest {
+    public static void testAddSupplier() {
+        DBProduct  dbProduct = new DBProduct(new DBController(new Controller()));
+        if(dbProduct.addSupplier("OJ", "555-55-55", "Cali Prob", "Pang pang")) {
+            System.out.println("hell yeah");
+        }
+
+    }
+    public static void main(String[] args) {
+        testAddSupplier();
+    }
+}
