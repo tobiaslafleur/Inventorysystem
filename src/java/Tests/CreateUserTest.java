9b911a903c7395d4260c41cad13e8772deda3702
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-14 18:38:55 +0200

    name change (#31)

--- src/java/Tests/CreateUserTest.java
+++ src/java/Tests/CreateUserTest.java
@@ -5,7 +5,7 @@ import Controller.DBController;
 import Controller.Database.DBUser;
 import Model.User;
 
-public class CreateUserSQLTest {
+public class CreateUserTest {
     public static void main(String[] args) {
         DBUser dbUser = new DBUser(new DBController(new Controller()));
         User user = new User("Orenthal James", "murderer", "pang pang", "555-55-55", "Why not in jail");
