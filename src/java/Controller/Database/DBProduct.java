d29125d00c9d043877a3f47e159f01e8d872d91c
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-14 11:58:07 +0200

    Added tests and SQL
    Added tests for AddProduct
    Added test for CreateUser
    Added AddProduct SQL query

--- src/java/Controller/Database/DBProduct.java
+++ src/java/Controller/Database/DBProduct.java
@@ -2,6 +2,7 @@ package Controller.Database;
 
 import Controller.DBController;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -33,10 +34,41 @@ public class DBProduct {
             conn.close();
             dbController.disconnect();
             return true;
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return false;
 
     }
+
+    public boolean addProduct(String name, int stock, BigDecimal price, int categoryID, String shelfPosition, int supplierID, BigDecimal cost, int userID) {
+        try {
+            dbController.connect();
+            Connection conn = dbController.getConnection();
+
+            String query =
+                    "INSERT INTO Product(name, stock, price, category_id, shelf_pos, supplier_id, cost, user_id) " +
+                            "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, stock);
+            preparedStatement.setBigDecimal(3, price);
+            preparedStatement.setInt(4, categoryID);
+            preparedStatement.setString(5, shelfPosition);
+            preparedStatement.setInt(6, supplierID);
+            preparedStatement.setBigDecimal(7, cost);
+            preparedStatement.setInt(8, userID);
+
+            preparedStatement.execute();
+            preparedStatement.close();
+            conn.close();
+            dbController.disconnect();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
---
+++ src/java/Tests/AddProductTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import Controller.Controller;
+import Controller.DBController;
+import Controller.Database.DBProduct;
+
+import java.math.BigDecimal;
+
+public class AddProductTest {
+    public static void main(String[] args) {
+        DBProduct dbProduct = new DBProduct(new DBController(new Controller()));
+        if(dbProduct.addProduct("PS5", 5, new BigDecimal("5999.99"), 1, "somewhere", 2, new BigDecimal("2999.99"), 2)) {
+            System.out.println("Done Worked");
+        }
+    }
+}
