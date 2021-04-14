3ee9ac5dcb3904ad24246259b85d3664555ea144
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-14 09:49:57 +0200

    Added constructor and instance of DBController

--- src/java/Controller/Database/DBProduct.java
+++ src/java/Controller/Database/DBProduct.java
@@ -1,4 +1,12 @@
 package Controller.Database;
 
+import Controller.DBController;
+
 public class DBProduct {
+    private DBController dbController;
+
+    public DBProduct(DBController dbController) {
+        this.dbController = dbController;
+    }
+    
 }
