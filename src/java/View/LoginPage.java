a7f16528ead5e275d0713066fd19edc6699e6e21
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-29 17:55:43 +0200

    removed imports

--- src/java/View/LoginPage.java
+++ src/java/View/LoginPage.java
@@ -3,17 +3,12 @@ package View;
 import Controller.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import javax.swing.*;
-import java.io.IOException;
+
 
 public class LoginPage {
     private static LoginPage instance;
