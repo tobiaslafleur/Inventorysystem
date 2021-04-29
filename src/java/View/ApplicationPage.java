3c20769eb882af2070943a364b4c76613c42275b
Satya Singh <80265446+SatyaSingh037@users.noreply.github.com>
2021-04-29 18:06:38 +0200

    Product table (#38)
    * Updated TableView in application page
    
    * removed imports
    
    * Updated applicationpage
    
    * again
    
    Co-authored-by: Tobias la Fleur <47904792+tobiaslafleur@users.noreply.github.com>

--- src/java/View/ApplicationPage.java
+++ src/java/View/ApplicationPage.java
@@ -1,6 +1,7 @@
 package View;
 
 import Controller.Main;
+import Model.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +11,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
@@ -18,8 +20,21 @@ import java.io.IOException;
 public class ApplicationPage {
     private static ApplicationPage instance;
     private GUIFacilitator facilitator;
+
+
+    @FXML private TableView<String> infoTable;
+    @FXML private TableColumn<Product, String> colName;
+    @FXML private TableColumn<Product, String> colQuantity;
+    @FXML private TableColumn<Product, String> colPrice;
+    @FXML private TableColumn<Product, String> colCategory;
+    @FXML private TableColumn<Product, String> colShelf;
+    @FXML private TableColumn<Product, String> colSupplier;
+    @FXML private TableColumn<Product, String> colCost;
+    
+
     @FXML private TableView<String> infoTable;
     private ObservableList<String> data;
+
 
     @FXML public void initialize() {
         instance = this;
