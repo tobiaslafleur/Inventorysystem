f3cc4d810ec5a07b89e26edbdd0021bda6cdee84
Tobias la Fleur <tobias.lafleur98@gmail.com>
2021-05-10 16:27:35 +0200

    Added password hashing

---
+++ src/java/Model/HashPassword.java
@@ -0,0 +1,27 @@
+package Model;
+
+import java.security.MessageDigest;
+
+public class HashPassword {
+
+    public static String hash(String username, String password) {
+        return sha256(username + password);
+    }
+
+    public static String sha256(String str) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(str.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+}
