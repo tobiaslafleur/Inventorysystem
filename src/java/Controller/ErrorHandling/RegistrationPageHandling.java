40025703b21691bbf7088692ed8eb959e5089274
unganemo <76483254+unganemo@users.noreply.github.com>
2021-05-12 23:24:22 +0200

    Implemented error-handling on registration page
    Still stuff to do, but created the basics

---
+++ src/java/Controller/ErrorHandling/RegistrationPageHandling.java
@@ -0,0 +1,113 @@
+package Controller.ErrorHandling;
+
+import java.util.ArrayList;
+
+public class RegistrationPageHandling {
+
+    private static boolean passwordOk = true;
+    private static boolean usernameOk = true;
+    private static boolean addressOk = true;
+    private static boolean emailOk = true;
+    private static boolean repeatPwOk = true;
+    private static boolean phoneOk = true;
+
+    private static final int PASSWORD_MIN_LENGTH = 8;
+    private static ArrayList<String> warnings = new ArrayList<>();
+
+
+    public static ArrayList<String> errorHandling(String username, String email, String phone, String address, String password, String repeated) {
+
+        if(!warnings.isEmpty()) {
+            warnings.clear();
+        }
+
+        //username
+        if(!usernameExists(username)){
+            warnings.add("Username already used");
+            usernameOk = false;
+        }
+
+        //check password
+        if(password.length() < PASSWORD_MIN_LENGTH) {
+            warnings.add("Password must be at least 8 characters long");
+            passwordOk = false;
+        }
+
+        if(!isPasswordValid(password)) {
+            warnings.add("Password contain at least one uppercase letter and one number");
+            passwordOk = false;
+        }
+
+        //check email
+        if(!email.contains("@")){
+            warnings.add("Enter a valid email");
+            emailOk = false;
+        }
+
+        if(!repeated.equals(password)) {
+            warnings.add("Passwords must match");
+            repeatPwOk = false;
+        }
+
+        try{
+            Integer.parseInt(phone);
+        } catch (NumberFormatException e) {
+            warnings.add("Enter a valid number");
+            phoneOk = false;
+        }
+
+
+        if(warnings.isEmpty()){
+            return null;
+        } else {
+            return warnings;
+        }
+
+    }
+
+    private static boolean usernameExists(String username) {
+        //TODO: Implement check username function
+        return true;
+    }
+
+    private static boolean isPasswordValid(String password) {
+        int charCount = 0;
+        int numCount = 0;
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if(ch >= '0' && ch <= '9') {
+                numCount++;
+            }
+            else if(ch >= 'A' && ch <= 'Z') {
+                charCount++;
+            }
+        }
+
+        return charCount >= 1 && numCount >= 1;
+    }
+
+    public static boolean isPasswordOk() {
+        return passwordOk;
+    }
+
+    public static boolean isUsernameOk() {
+        return usernameOk;
+    }
+
+    public static boolean isAddressOk() {
+        return addressOk;
+    }
+
+    public static boolean isEmailOk() {
+        return emailOk;
+    }
+
+    public static boolean isRepeatPwOk() {
+        return repeatPwOk;
+    }
+
+    public static boolean isPhoneOk() {
+        return phoneOk;
+    }
+}
